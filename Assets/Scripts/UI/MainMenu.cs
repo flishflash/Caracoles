ccf8a7fd6e7e19b487a6862f2ff0e63b13a3b45f
Hacienda69 <fannumero1dehacienda@gmail.com>
2024-05-15 12:18:25 +0200

    la más mala del frustum soy yo

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
