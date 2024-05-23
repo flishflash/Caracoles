87ace153176ca96362ab970fe74014d49bb30d19
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2024-05-23 12:13:58 +0200

    biatch (animacion de crear trampas)

--- Assets/Scripts/Traps/TrapBehaviour.cs
+++ Assets/Scripts/Traps/TrapBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TrapBehaviour : MonoBehaviour
@@ -8,11 +9,47 @@ public class TrapBehaviour : MonoBehaviour
     public int dmg;
     public float animationSpeed;
 
-    void Update()
+    private bool landed = false;
+
+    private void Start()
     {
-        DoAnimation();
+        StartCoroutine(StartAnimation());
     }
 
+    void Update()
+    {
+        if (landed) DoAnimation();
+    }
+
+    /// <summary>
+    /// Before landing
+    /// </summary>
+    public IEnumerator StartAnimation()
+    {
+        Vector3 initialScale = transform.localScale;
+        float initialHeight = transform.localPosition.y;
+
+        transform.localPosition = transform.localPosition + new Vector3(0, 10, 0);
+
+        float time = 0;
+        float landingTime = 0.25f;
+
+        while (time <= landingTime) 
+        {
+            float currentHeight = Mathf.Lerp(initialHeight + 10, initialHeight, time * (1 / landingTime));
+            transform.localPosition = new Vector3(transform.localPosition.x, currentHeight, transform.localPosition.z);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = new Vector3(transform.localPosition.x, initialHeight, transform.localPosition.z);
+        landed = true;
+    }
+
+    /// <summary>
+    /// After landing
+    /// </summary>
     public virtual void DoAnimation()
     {
 
